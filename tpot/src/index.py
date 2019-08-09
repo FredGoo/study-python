6e0a6544ae807b8b501f5aa51065c212c835babb
guye <guye@geexfinance.com>
2019-08-09 11:54:25 +0800

    短信监控

--- tpot/src/index.py
+++ tpot/src/index.py
@@ -1,3 +1,5 @@
+# encoding:utf-8
+
 from tpot import TPOTClassifier
 
 
@@ -5,7 +7,7 @@ def start():
     pipeline_optimizer = TPOTClassifier(generations=5, population_size=20, cv=5,
                                         random_state=42, verbosity=2)
 
-    pipeline_optimizer.fit('x_train', 'y_train')
+    pipeline_optimizer.fit([['x']], ['y'])
 
 
 if __name__ == '__main__':
