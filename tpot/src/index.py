bb9f345100e4ff859359bcd7e168e668f8817795
guye <guye@geexfinance.com>
2019-09-10 09:17:22 +0800

    tpot test

--- tpot/src/index.py
+++ tpot/src/index.py
@@ -2,18 +2,16 @@
 import csv
 
 import numpy as np
-import pydotplus
-from sklearn import tree
 from sklearn.model_selection import train_test_split
 from tpot import TPOTClassifier
 
 
 def start():
-    with open('/home/fred/Documents/rmd/antifraud/out/app.csv') as f:
+    with open('/home/fred/Documents/2.rmd/1.antifraud/out/app.csv') as f:
         data = list(csv.reader(f))
         del data[0]
 
-    with open('/home/fred/Documents/rmd/antifraud/out/app_result.csv') as f:
+    with open('/home/fred/Documents/2.rmd/1.antifraud/out/app_result.csv') as f:
         target = list(csv.reader(f))
         del target[0]
 
@@ -27,20 +25,9 @@ def start():
 
     X_train, X_test, y_train, y_test = train_test_split(data_np1, target_np1,
                                                         train_size=0.75, test_size=0.25)
-    # pipeline_optimizer.fit(X_train, y_train)
-    # print(pipeline_optimizer.score(X_test, y_test))
-    # pipeline_optimizer.export('tpot_exported_pipeline.py')
-
-    exported_pipeline = tree.DecisionTreeClassifier(criterion="gini", max_depth=7, min_samples_leaf=11,
-                                                    min_samples_split=12)
-    exported_pipeline.fit(X_train, y_train)
-    tree.export_graphviz(exported_pipeline, out_file='tree.dot')
-    results = exported_pipeline.predict(X_test)
-    print(results)
-
-    dot_data = tree.export_graphviz(exported_pipeline, out_file=None)
-    graph = pydotplus.graph_from_dot_data(dot_data)
-    graph.write_jpg("tree.jpg")  # 生成jpg文件
+    pipeline_optimizer.fit(X_train, y_train)
+    print(pipeline_optimizer.score(X_test, y_test))
+    pipeline_optimizer.export('tpot_exported_pipeline.py')
 
 
 if __name__ == '__main__':
--- tpot/src/tpot_exported_pipeline.py
+++ tpot/src/tpot_exported_pipeline.py
@@ -9,7 +9,7 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Average CV score on the training set was:0.8964285714285716
+# Average CV score on the training set was:0.8412698412698413
 exported_pipeline = DecisionTreeClassifier(criterion="gini", max_depth=7, min_samples_leaf=11, min_samples_split=12)
 
 exported_pipeline.fit(training_features, training_target)
