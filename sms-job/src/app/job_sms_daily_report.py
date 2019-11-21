e5af8d3889a5c58bec12f5d0873573188c11107b
guye <guye@geexfinance.com>
2019-11-21 14:56:12 +0800

    短信报告

--- sms-job/src/app/job_sms_daily_report.py
+++ sms-job/src/app/job_sms_daily_report.py
@@ -1,21 +1,18 @@
 # -*- coding: utf-8 -*-
+import codecs
 import csv
 import json
 import smtplib
-import xml.etree.ElementTree as ET
+from datetime import datetime, timedelta
 from email.header import Header
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
 
+import Levenshtein
 import pymysql
-import requests
 
-es_host = 'http://192.168.100.100:9200'
-es_url = 'http://192.168.100.100:9200/logstash-2019.11.10/_search'
-headers = {'Content-Type': 'application/json'}
-auth = ('itguest', 'Geex2016')
-smsmap = {
+type_map = {
     1: "超G会员",
     2: "超即花",
     3: "催收提醒",
@@ -26,39 +23,7 @@ smsmap = {
     8: "现金贷",
     9: "验证码"
 }
-
-huaxin_config = {
-    'tongzhi': {
-        'url': 'http://dx.ipyy.net/statusApi.aspx',
-        'userid': '60506',
-        'account': '9B000153',
-        'password': 'C1879E192918B907B508E926224DF8A2',
-    },
-    # 'yingxiao': {
-    #     'userid': '',
-    #     'account': '9BYX00117',
-    #     'password': '',
-    # },
-    'yanzhengma': {
-        'url': 'http://dx.ipyy.net/statusApi.aspx',
-        'userid': '60506',
-        'account': '9B000153',
-        'password': 'C1879E192918B907B508E926224DF8A2',
-    },
-    'wangdai': {
-        'url': 'http://dx110.ipyy.net/statusApi.aspx',
-        'userid': '7199',
-        'account': '9BWD00036',
-        'password': '4CE6B7F65C369BE4EA123EA1F22FBE8F',
-    },
-    'cuishou': {
-        'url': 'http://dx110.ipyy.net/statusApi.aspx',
-        'userid': '7198',
-        'account': '9BYX00107',
-        'password': 'DFC3B6AC521781C98886F95504523F9B',
-    }
-}
-huaxin_map = {}
+template_map = []
 
 
 def connect_mysql_db(database):
@@ -73,176 +38,18 @@ def connect_mysql_db(database):
                            charset='utf8')
 
 
-def query_huaxin_es():
-    smscsv = {}
+def query_sms_result(start, end):
+    res = {}
+    header = {'渠道': None,
+              '类型': None,
+              '总数': None,
+              '送达': None,
+              '其他': None,
+              '未知': None}
 
-    # 根据华信条件获取日志
-    query = '''
-{
-	"query": {
-		"bool": {
-			"must": [{
-					"match": {
-						"app_name": "MessagePusher"
-					}
-				},
-								{
-					"match": {
-						"message": "HuaxinSmsService"
-					}
-				},
-				{
-					"match": {
-						"message": "taskID"
-					}
-				}
-			]
-		}
-	},
-    "_source": {
-        "includes": [
-            "thread",
-            "message",
-            "timestamp",
-            "host_name"
-        ]
-    },
-    "sort" : ["_doc"], 
-    "size": 100
-}
-    '''
-    res = requests.post(url=es_url + '?scroll=10m', headers=headers, data=query, auth=auth)
-    resJson = json.loads(res.content)
-    scrollid = resJson['_scroll_id']
-    print(resJson)
-
-    # 根据线程号获取短信内容
-    for hit in resJson['hits']['hits']:
-        smscsv = process_huaxin_hit(hit, smscsv)
-
-    # scroll遍历
-    breakflag = True
-    while breakflag:
-        scrollquery = '{"scroll": "10m", "scroll_id": "%s"}' % scrollid
-        scrollres = requests.post(url=es_host + '/_search/scroll', headers=headers, data=scrollquery, auth=auth)
-        scrollresJson = json.loads(scrollres.content)
-        scrollid = scrollresJson['_scroll_id']
-        print(scrollresJson)
-
-        # 根据线程号获取短信内容
-        for hit in scrollresJson['hits']['hits']:
-            smscsv = process_huaxin_hit(hit, smscsv)
-
-        if len(scrollresJson['hits']['hits']) < 1:
-            breakflag = False
-
-    return smscsv
-
-
-def process_huaxin_hit(hit, smscsv):
-    print(hit['_source'])
-    # 获取线程号
-    thread_no = hit['_source']['thread'].split("#")[1].split("-")[1]
-    # 获取taskid
-    task_message = hit['_source']['message'].replace('HuaxinSmsService end: ', '')
-    task_result = json.loads(task_message)
-    taskid = task_result['taskID']
-    if taskid == '':
-        return smscsv
-
-    # 获取hostname
-    hostname = hit['_source']['host_name']
-
-    query = '''
-    {
-    	"query": {
-    		"bool": {
-    			"must": [{
-    					"match": {
-    						"app_name": "MessagePusher"
-    					}
-    				},
-    				{
-    					"match": {
-    						"thread": "%s"
-    					}
-    				},
-    				{
-    					"match": {
-    						"host_name": "%s"
-    					}
-    				},
-    				{
-    					"range": {
-    						"timestamp": {
-    							"lt": %s
-    						}
-    					}
-    				}
-    			]
-    		}
-    	},
-    	"sort": {
-    		"timestamp": {
-    			"order": "desc"
-    		}
-    	},
-    	    "_source": {
-            "includes": [
-                "thread",
-                "message"
-            ]
-        },
-    	"size": 2
-    }
-            '''
-    query = query % (thread_no, hostname, hit['_source']['timestamp'])
-    res1 = requests.post(url=es_url, headers=headers, data=query, auth=auth)
-    resJson1 = json.loads(res1.content)
-
-    for hit1 in resJson1['hits']['hits']:
-        if not hit1['_source']['message'].__contains__('channelConfig'):
-            hit1message = hit1['_source']['message'].replace('Send Sms Message START: ', '')
-
-            try:
-                qresstatus, qresbusinessid = query_huaxin_by_taskid(taskid, json.loads(hit1message)['templateId'])
-                if smscsv.__contains__(qresbusinessid) and smscsv[qresbusinessid].__contains__(qresstatus):
-                    smscsv[qresbusinessid][qresstatus] += 1
-                else:
-                    smscsv[qresbusinessid] = {
-                        qresstatus: 1
-                    }
-            except:
-                print('process_huaxin_hit error', hit1['_source'])
-
-    return smscsv
-
-
-def query_huaxin_by_taskid(taskid, templateid):
-    mapconfig = huaxin_map[templateid]
-    queryconfig = huaxin_config[mapconfig['C_CHANNEL_ID']]
-    data = {
-        'userid': queryconfig['userid'],
-        'account': queryconfig['account'],
-        'password': queryconfig['password'],
-        'statusNum': 1,
-        'action': 'query',
-        'taskid': taskid
-        # 'taskid': '1911044500010583'
-    }
-    response = requests.get(url=queryconfig['url'], params=data)
-    xml = ET.fromstring(response.content)
-    if len(xml) > 0:
-        status = xml[0][2].text
-    else:
-        status = 999
-    return status, mapconfig['N_BUSINESS_ID']
-
-
-def load_huaxin_sms_vendoer_map():
     sql = '''
-        select C_TEMPLATE_ID, C_CHANNEL_ID, N_BUSINESS_ID from GEEX_SMS_TEMPLATE where N_BUS_TYPE = 8;
-    '''
+    select * from GEEX_SMS_RECORD r left join GEEX_SMS_REPORT p on r.TASK_ID = p.SEQ_ID where r.D_CREATE between '%s' and '%s'
+    ''' % (start, end)
 
     con = connect_mysql_db('datacenter')
     cur = con.cursor(cursor=pymysql.cursors.DictCursor)
@@ -252,7 +59,76 @@ def load_huaxin_sms_vendoer_map():
     con.close()
 
     for row in rows:
-        huaxin_map[row['C_TEMPLATE_ID']] = row
+        # 寻找短信模板
+        sendmsgcontent = row['C_MSG']
+        print('发送短信内容', sendmsgcontent)
+
+        mindis = 99999
+        templaterow = {}
+        for template in template_map:
+            templatecontent = template['C_CONTENT']
+
+            dis = Levenshtein.distance(sendmsgcontent, templatecontent)
+            if dis < mindis:
+                mindis = dis
+                templaterow = template
+
+        print('数据库中模板', templaterow['C_CONTENT'], '\n')
+
+        # 封装返回
+        bustype = row['MSG_TYPE']
+        if templaterow['N_BUSINESS_ID'] != None and type_map.__contains__(templaterow['N_BUSINESS_ID']):
+            msgtype = type_map[templaterow['N_BUSINESS_ID']]
+        else:
+            msgtype = templaterow['N_BUSINESS_ID']
+        smsstate = row['STATE']
+        if smsstate == None or smsstate == '':
+            smsstate = '未知'
+
+        if bustype != None and msgtype != None:
+            if not res.__contains__(bustype + msgtype):
+                res[bustype + msgtype] = {
+                    '渠道': bustype,
+                    '类型': msgtype,
+                    '总数': 0,
+                    '送达': 0,
+                    '其他': 0,
+                    '未知': 0
+                }
+            if not res[bustype + msgtype].__contains__(smsstate):
+                res[bustype + msgtype][smsstate] = 1
+            else:
+                res[bustype + msgtype][smsstate] += 1
+
+            # 归类
+            if smsstate == 'DELIVRD':
+                res[bustype + msgtype]['送达'] += 1
+            elif smsstate != '未知':
+                res[bustype + msgtype]['其他'] += 1
+
+            # 总数
+            res[bustype + msgtype]['总数'] += 1
+
+            # 文件头
+            if not header.__contains__(smsstate):
+                header[smsstate] = None
+
+    return res, header
+
+
+def load_sms_vendoer_map():
+    sql = '''
+        select * from GEEX_SMS_TEMPLATE;
+    '''
+
+    con = connect_mysql_db('datacenter')
+    cur = con.cursor(cursor=pymysql.cursors.DictCursor)
+    cur.execute(sql)
+    rows = cur.fetchall()
+    cur.close()
+    con.close()
+
+    return rows
 
 
 def _format_addr(s):
@@ -260,25 +136,15 @@ def _format_addr(s):
     return formataddr((Header(name, 'utf-8').encode(), addr))
 
 
-def sendemail(data):
-    csv_file = open('sms.csv', 'w')
-    csvwriter = csv.DictWriter(csv_file, ['渠道', '类型', '成功', '失败'])
+def sendemail(header, data):
+    csv_file = codecs.open('sms.csv', 'w', 'gbk')
+    csvwriter = csv.DictWriter(csv_file, header.keys())
     csvwriter.writeheader()
-
-    for key in data.keys():
-        csvrow = {
-            '渠道': '华信',
-            '类型': smsmap[key]
-        }
-        for keykey in data[key]:
-            if keykey == '10':
-                csvrow['成功'] = data[key][keykey]
-            else:
-                csvrow['失败'] = data[key][keykey]
-
-        csvwriter.writerow(csvrow)
-
+    csvwriter.writerows(data.values())
     csv_file.close()
+
+    # 测试
+    # return
 
     from_addr = 'noreply@geexfinance.com'
     pwd = 'Geexfcs1234'
@@ -309,6 +175,13 @@ def sendemail(data):
 
 
 if __name__ == '__main__':
-    load_huaxin_sms_vendoer_map()
-    hxres = query_huaxin_es()
-    sendemail(hxres)
+    template_map = load_sms_vendoer_map()
+
+    dt = datetime.today()
+    end = dt.strftime('%Y-%m-%d')
+    startday = dt - timedelta(days=1)
+    start = startday.strftime('%Y-%m-%d')
+
+    res, header = query_sms_result(start, end)
+
+    sendemail(header, res)
